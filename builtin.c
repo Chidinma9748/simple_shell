d9c38388da119f5ad05f596af9d38d6a7111daa3
Chidinma9748 <chidinmaukachi20@gmail.com>
2023-11-15 16:31:01 -0800

    updated

---
+++ builtin.c
@@ -0,0 +1,97 @@
+#include "shell.h"
+
+/**
+ * quit - exits the shell
+ * @data: Structure containing potential arguments. Used to maintain
+ *          constant function prototype.
+ *  Return: exits with a given exit status
+ *         (0) if data.argv[0] != "exit"
+ */
+int quit(data_t *data)
+{
+	int exitcheck;
+
+	if (data->argv[1])  /* If there is an exit arguement */
+	{
+		exitcheck = _erratoi(data->argv[1]);
+		if (exitcheck == -1)
+		{
+			data->status = 2;
+			print_error(data, "Wrong number: ");
+			_eputs(data->argv[1]);
+			_eputchar('\n');
+			return (1);
+		}
+		data->err_num = _erratoi(data->argv[1]);
+		return (-2);
+	}
+	data->err_num = -1;
+	return (-2);
+}
+
+/**
+ * change - changes the current directory of the process
+ * @data: Structure containing potential arguments. Used to maintain
+ *          constant function prototype.
+ *  Return: Always 0
+ */
+int change(data_t data)
+{
+	char *s, *dir, buffer[1024];
+	int chdir_ret;
+
+	s = getcwd(buffer, 1024);
+	if (!s)
+		_puts("TODO: >>getcwd failure emsg here<<\n");
+	if (!data->argv[1])
+	{
+		dir = _getenv(data, "HOME=");
+		if (!dir)
+			chdir_ret = /* TODO: what should this be? */
+				chdir((dir = _getenv(data, "PWD=")) ? dir : "/");
+		else
+			chdir_ret = chdir(dir);
+	}
+	else if (_strcmp(data->argv[1], "-") == 0)
+	{
+		if (!_getenv(data, "OLDPWD="))
+		{
+			_puts(s);
+			_putchar('\n');
+			return (1);
+		}
+		_puts(_getenv(data, "OLDPWD=")), _putchar('\n');
+		chdir_ret = /* TODO: what should this be? */
+			chdir((dir = _getenv(data, "OLDPWD=")) ? dir : "/");
+	}
+	else
+		chdir_ret = chdir(data->argv[1]);
+	if (chdir_ret == -1)
+	{
+		print_error(data, "can't cd to ");
+		_eputs(data->argv[1]), _eputchar('\n');
+	}
+	else
+	{
+		_setenv(data, "OLDPWD", _getenv(data, "PWD="));
+		_setenv(data, "PWD", getcwd(buffer, 1024));
+	}
+	return (0);
+}
+
+/**
+ * _myhelp - changes the current directory of the process
+ * @data: Structure containing potential arguments. Used to maintain
+ *          constant function prototype.
+ *  Return: Always 0
+ */
+int _myhelp(data_t data)
+{
+	char **arg_array;
+
+	arg_array = data->argv;
+	_puts("help call works. Function not yet implemented \n");
+	if (0)
+		_puts(*arg_array); /* temp att_unused workaround */
+	return (0);
+}
